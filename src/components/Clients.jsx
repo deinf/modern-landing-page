dfc07453db07c3a20f8d79b8ac643fceaf5c908f
Danang Eka Saputra <danangekasaputra@outlook.com>
2023-03-21 16:57:28 +0700

    add clients page and cta page

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import { clients } from '../constants'
+import styles from '../style'
 const Clients = () => {
   return (
-    <div>Clients</div>
+   <section className={`${styles.flexCenter} my-4`}>
+
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+          {clients.map(client => (
+            <div className={`${styles.flexCenter} flex-1 sm:min-w-[192px] min-w-[120px]`} key={client.id}>
+              <img src={client.logo} alt="client" className='sm:w-[192px] w-[100px] object-contain'/>
+             
+            </div>
+          ))}
+        
+      </div>
+   </section>
   )
 }
 
